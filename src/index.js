8e37827c3f6cb4bb172282b77b895a872a671ba9
sadra <sadrataghavi1383@gmail.com>
2021-07-12 20:19:42 +0430

    importing files fom components folder and linking them to ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+// components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function Main(props){
     return(
-        <div>solam</div>
+        <div className="App">
+            <header>
+                <h1>React Todo List</h1>
+            </header>
+            <Form />
+            <TodoList />
+        </div>
     )
 }
 
