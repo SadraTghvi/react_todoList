6bff790549a7beb0146fbe015d15bf5e6eb98717
sadra <sadrataghavi1383@gmail.com>
2021-07-12 19:53:41 +0430

    initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function Main(props){
+    return(
+        <div>solam</div>
+    )
+}
+
+ReactDOM.render(
+    <Main />,
+  document.getElementById('root')
+);
