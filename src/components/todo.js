e6577519b39ae2973480575003b2fa5f1afe2724
sadra <sadrataghavi1383@gmail.com>
2021-07-13 14:47:56 +0430

    working with props

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Todo(props){
     return(
         <div className="todo">
-            <li className="todo-item">solam</li>
+            <li className="todo-item">{props.text}</li>
             <button className="complete-btn"><i className="fas fa-check"></i></button>
             <button className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
