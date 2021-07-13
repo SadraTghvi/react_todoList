5740990fa6f7adfb9b0d366abf15b5097f019737
sadra <sadrataghavi1383@gmail.com>
2021-07-13 16:07:10 +0430

    function if the task is completed

--- src/components/todo.js
+++ src/components/todo.js
@@ -8,11 +8,23 @@ function Todo(props){
         }
         ));
     }
+
+    const completeTodo = () =>{
+        props.setTodos(props.todos.map((item) =>{
+            if(item.id === props.todo.id){
+                return{
+                    ...item,completed: !item.completed,
+                };
+            }
+            return item;
+        }))
+    }
+
     return(
         <div className="todo">
             <li className="todo-item">{props.text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
-            <button  className="trash-btn" onClick={deleteTodo}><i className="fas fa-trash"></i></button>
+            <button className="complete-btn" onClick={completeTodo}><i className="fas fa-check"></i></button>
+            <button className="trash-btn"    onClick={deleteTodo}><i className="fas fa-trash"></i></button>
         </div>
     )
 }
