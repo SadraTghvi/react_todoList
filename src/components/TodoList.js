bd0bcc7acab94f990dfa2b35e5c2ba10be860e5b
sadra <sadrataghavi1383@gmail.com>
2021-07-15 15:18:47 +0430

    showing them on UI

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@ function TodoList(props){
     return(
         <div className="todo-container">
             <ul className="todo-list">
-                {props.todos.map(todo =>(
+                {props.filteredTodos.map(todo =>(
                     <Todo setTodos={props.setTodos} todos={props.todos} todo={todo} key={todo.id} text={todo.text}/>
                 ))}
             </ul>
