4898437bf03f93f6205b59252fd8bed284a67d86
sadra <sadrataghavi1383@gmail.com>
2021-07-15 15:09:19 +0430

    functionality for status

--- src/index.js
+++ src/index.js
@@ -9,13 +9,28 @@ function Main(props){
     const [inputText,setInputText] = useState("");
     const [todos,setTodos] = useState([])
     const [status,setStatus] = useState("all")
+    const [filteredTodos, setFilteredTodos] = useState([])
+
+    const filterHandler = () =>{
+        switch(status){
+            case "completed":
+                setFilteredTodos(todos.filter(todo => todo.completed === true));
+                break;
+            case "uncompleted":
+                setFilteredTodos(todos.filter(todo => todo.uncompleted === true));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    }
 
     return(
         <div className="App">
             <header>
                 <h1>React Todo List </h1>
             </header>
-            <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+            <Form setStatus={setStatus} inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
             <TodoList setTodos={setTodos} todos={todos}/>
         </div>
     )
